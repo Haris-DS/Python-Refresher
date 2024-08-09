df5128a2588730788f2e7bd6da41a51770b0e083
Haris Habib <harishabib2@gmail.com>
2024-08-09 17:13:26 +0500

    USD to EURO Convertor

--- Basics/basic.py
+++ Basics/basic.py
@@ -1,3 +1,46 @@
 # this is Comment
 print("Python is Fun!!!")
 print("Hurray")
+#  Data Types String and Integer
+name = ("Darth vader")
+print(name)
+age = 19
+print(age)
+
+# Float
+
+pi = (3.41)
+print(pi)
+type(pi)
+
+# Boolean
+
+bool_example = True
+print(bool_example)
+type(bool_example)
+
+I_cannot_do_it = False
+print(I_cannot_do_it)
+
+# Type Castng
+
+age = "100"
+type(age)
+print(int(age))
+height = "1.09"
+print(height)
+print(float(height))
+new_height = float(height) + 0.66
+print(new_height)
+
+# USD to EURO Convertor
+
+usd_amout = 100
+
+exchange_rate = 0.92
+
+eur_amount = usd_amout * exchange_rate
+
+eur_amount = int(eur_amount)
+
+print("The Amount in EURO is:", eur_amount)

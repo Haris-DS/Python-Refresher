246b560c1a63ab0742fd274014857ea6ac6e91fa
Haris Habib <harishabib2@gmail.com>
2024-08-12 15:12:19 +0500

    Level 3 Is Completed

--- Basics/basic.py
+++ Basics/basic.py
@@ -1,46 +1,98 @@
 # this is Comment
-print("Python is Fun!!!")
-print("Hurray")
+
+# print("Python is Fun!!!")
+# print("Hurray")
+
 #  Data Types String and Integer
-name = ("Darth vader")
-print(name)
-age = 19
-print(age)
+
+# name = ("Darth vader")
+# print(name)
+# age = 19
+# print(age)
 
 # Float
 
-pi = (3.41)
-print(pi)
-type(pi)
+# pi = (3.41)
+# print(pi)
+# type(pi)
 
 # Boolean
 
-bool_example = True
-print(bool_example)
-type(bool_example)
+# bool_example = True
+# print(bool_example)
+# type(bool_example)
 
-I_cannot_do_it = False
-print(I_cannot_do_it)
+# I_cannot_do_it = False
+# print(I_cannot_do_it)
 
 # Type Castng
 
-age = "100"
-type(age)
-print(int(age))
-height = "1.09"
-print(height)
-print(float(height))
-new_height = float(height) + 0.66
-print(new_height)
+# age = "100"
+# type(age)
+# print(int(age))
+# height = "1.09"
+# print(height)
+# print(float(height))
+# new_height = float(height) + 0.66
+# print(new_height)
 
 # USD to EURO Convertor
 
-usd_amout = 100
+# usd_amout = 100
 
-exchange_rate = 0.92
+# exchange_rate = 0.92
 
-eur_amount = usd_amout * exchange_rate
+# eur_amount = usd_amout * exchange_rate
 
-eur_amount = int(eur_amount)
+# eur_amount = int(eur_amount)
 
-print("The Amount in EURO is:", eur_amount)
+# print("The Amount in EURO is:", eur_amount)
+
+# Maths In Python
+# Arthicmatic Rules
+
+# age = 44.4
+# add 1 and multiply with 2 and added in age
+# print(age+1*2)
+
+# Multiple age with 2
+# print((age+1)*2)
+
+# Power
+# print((age+1)**2)
+
+# Heights
+# ahsoka_height = float(1.7)
+# yoda_height = float(0.66)
+# r2d2_height = float(1.09)
+# c3po_height = float(1.75)
+#  total variables divided by 4 
+# print((ahsoka_height + yoda_height + r2d2_height + c3po_height) / 4)
+# String Concatination 
+# name = "Yoda"
+# print("Your name is "+name)
+# height = 0.66
+# age = 900
+# print("Your name, height and age is: " + name +"," + str(height) + "and age is "+ str(age))
+# print(type(name))
+# print(type(height))
+# print(type(age))
+
+
+# first_name = "Jack"
+# second_name = "Rose"
+# year = 1912
+
+# print(first_name + " and " + second_name + " were on the Titanic in "+ str(year)) 
+
+# Exercise
+""" You are an intern at a bank and you habe been tasked to create a simple program to calculte the interest that customer will earn on their saving account. The interest is calculated yearly using the simple interest formula 
+
+Simple interest = principal * rate * time / 100
+
+"""
+# principal = 1000
+# rate = 5
+# time = 2
+# interest = (principal * rate * time)/100
+# print(interest)
